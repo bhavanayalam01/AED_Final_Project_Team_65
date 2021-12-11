e4b979e276f8519a436e3dde5c12a6d63c11b0ea
skkotla <kotla.s@northeastern.edu>
2021-12-10 23:17:39 -0500

    ui

--- AEDFinalProject/src/ui/DoctorRole/RequestOrganJpanel.java
+++ AEDFinalProject/src/ui/DoctorRole/RequestOrganJpanel.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package userinterface.DoctorRole;
+package ui.DoctorRole;
 
 import Business.DB4OUtil.DB4OUtil;
 import Business.EcoSystem;
